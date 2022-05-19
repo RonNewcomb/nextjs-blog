f24bb41113fdaffb0805779dbb93d6ccdcbba0c8
Ron Newcomb <ron.newcomb@agilent.com>
2022-05-19 15:33:10 -0700

    component tests

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -9,6 +9,8 @@
 // https://on.cypress.io/plugins-guide
 // ***********************************************************
 
+const injectDevServer = require('@cypress/react/plugins/next');
+
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
@@ -19,4 +21,6 @@
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
-}
+  injectDevServer(on, config);
+  return config;
+};
